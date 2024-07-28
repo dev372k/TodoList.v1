f001a14cbafdc8c2c57d21e560e09268249ba7b6
S.M.Owais <Hafizh@lifejourney.ai>
2024-07-29 00:34:54 +0500

    Update TodoController.cs

--- API/Controllers/TodoController.cs
+++ API/Controllers/TodoController.cs
@@ -41,6 +41,17 @@ public class TodoController : ControllerBase
     {
         try
         {
+            var todo = todos.Any();
+            if (todo == false)
+            {
+                request.Id = 1;
+            }
+            else
+            {
+                var todoList = todos.OrderByDescending(_ => _.Id).FirstOrDefault();
+                request.Id = todoList.Id + 1;
+            }
+
             todos.Add(request);
             return Ok(new ResponseModel { Message = "Todo added successfully.", Data = request });
         }
@@ -49,7 +60,7 @@ public class TodoController : ControllerBase
             return Ok(new ResponseModel { Status = false, Message = ex.Message });
         }
     }
- 
+
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
@@ -61,6 +72,44 @@ public class TodoController : ControllerBase
 
             todos.Remove(todo);
             return Ok(new ResponseModel { Message = "Todo deleted successfully." });
+        }
+        catch (Exception ex)
+        {
+            return Ok(new ResponseModel { Status = false, Message = ex.Message });
+        }
+    }
+
+    //[HttpPut]
+    //public IActionResult Put(Todo request)
+    //{
+    //    try
+    //    {
+    //        var todo = todos.FirstOrDefault(x => x.Id == request.Id);
+    //        if (todo == null)
+    //            throw new Exception("There is no such todo found.");
+
+    //        todo.Title = request.Title;
+    //        todo.Description = request.Description;
+    //        return Ok(new ResponseModel { Message = "Todo updated successfully." });
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        return Ok(new ResponseModel { Status = false, Message = ex.Message });
+    //    }
+    //}
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Todo request)
+    {
+        try
+        {
+            var todo = todos.FirstOrDefault(x => x.Id == id);
+            if (todo == null)
+                throw new Exception("There is no such todo found.");
+
+            todo.Title = request.Title;
+            todo.Description = request.Description;
+            return Ok(new ResponseModel { Message = "Todo updated successfully." });
         }
         catch (Exception ex)
         {
